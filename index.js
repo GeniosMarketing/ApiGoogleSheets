76d611c7016c3c3327efde650f919b62a8347cad
Genios Marketing <geniosmarketinghn@gmail.com>
2023-02-10 15:53:17 -0600

    Subiendo Proyecto

---
+++ index.js
@@ -0,0 +1,60 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const {google} = require("googleapis");
+//const keys = require('./keys.json');
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+app.post("/", async (req, res)=>{
+  const { nombre, telefono, correo, metros, precio, total, constancia, empresa, rtn, pago, saldo} = req.body;
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "keys.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+   // Create client instance for auth
+   const client = await auth.getClient();
+
+
+   // Instance of Google Sheets API
+   const googleSheets = google.sheets({ version: "v4", auth: client });
+   const spreadsheetId = "1qr606M1U3pD_wqujj00Q95-uscmV4iA6sPcyPSnuUTQ";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+   // Get Rows from spreadcheets
+   const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Registro!A:K",
+   });
+
+   // Escribir en las Filas de la hoja de calculo
+   await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: "Registro!A:K",
+    valueInputOption: "USER_ENTERED",
+    resource: {
+        values: [
+            [nombre, telefono, correo, metros, precio, total, constancia, empresa, rtn, pago, saldo]],
+    },
+   });
+
+
+
+  res.send("Registros guardados correctamente...!");
+});
+
+app.listen(3000, (req, res) => console.log("Escuchando en el puerto 3000"));
